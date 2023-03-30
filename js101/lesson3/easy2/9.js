75feca480ed01195a445fb4e597763a0506aeba6
Hugo van der Horst <hugovndrhorst@gmail.com>
2023-03-30 19:59:54 +0200

    easy2

--- js101/lesson3/easy2/9.js
+++ js101/lesson3/easy2/9.js
@@ -1,3 +1,5 @@
 let title = "Flintstone Family Members";
 
-console.log(title.length);
+let padding = Math.floor((40 - title.length) / 2);
+console.log(padding);
+console.log(title.padStart(padding + title.length));
